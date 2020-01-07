d29b462ec09c803b1e9f3db9ebd77dcd5115cb50
ejazhussain1995 <ejazparvi6@gmail.com>
2020-01-07 09:36:57 -0500

    add shared folder which include UIElements

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-    return <h2>Users Works</h2>
+
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Max Schwarz',
+            image:'https://burst.shopifycdn.com/photos/leather-anchor-bracelet-for-men.jpg?width=373&format=pjpg&exif=0&iptc=0',
+            place: 3
+        }
+    ];
+    return (    
+        <UsersList items={USERS}/>
+    );
 };
 
 export default Users;
