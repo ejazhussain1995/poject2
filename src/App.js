028e6a72d0b52c08876ab6fa9e375675d00b5d67
ejazhussain1995 <ejazparvi6@gmail.com>
2020-01-15 06:27:09 -0500

    Wrap up frontend module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
@@ -6,33 +6,66 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlace from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth'
-import MainNavigation from './shared/components/Navigation/MainNavigation'; 
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
-  return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
             <Users />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlace />
-          </Route>
-          <Route path="/places/:placeId" exact>
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlace />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId" exact>
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />     
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+            <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlace />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          {routes}
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 
 const Auth = () => {
-    const [formState, inputHandler ] = useForm({
+    const auth = useContext(AuthContext);
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
+    const [formState, inputHandler, setFormData ] = useForm({
         email: {
             value: '',
             valid: 'false'
@@ -19,10 +23,31 @@ const Auth = () => {
             isValid: false
         }
     },false);
+
+    const switchModeHandler = () => {
+        if (!isLoginMode) {
+            setFormData(
+                {
+                    name: undefined
+                },
+                formState.inputs.email.isValid && formState.inputs.password.isValid);    
+        } else {
+            setFormData({
+                ...formState.inputs,
+                name: {
+                    value: '',
+                    isValid: false
+                }
+
+            }, false);
+        }
+        setIsLoginMode(prevMode => !prevMode);
+    };
     
     const authSubmitHandler = event => {
-        event.preventDefault();
+        event.preventDefault();  
         console.log(formState.inputs)
+        auth.login();
     }
 
     return (
@@ -30,6 +55,15 @@ const Auth = () => {
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && ( 
+                    <Input element="input" 
+                    id="name" type="text" 
+                    label="Your Name" 
+                    validators={[VALIDATOR_REQUIRE()]} 
+                    errorText="Please enter a name."
+                    onInput={inputHandler}
+                    />
+                )}
                 <Input
                     element="input"
                     id="email"
@@ -48,8 +82,9 @@ const Auth = () => {
                     errorText="Please enter a valid password, at least 8 characters."
                     onInput={inputHandler}
                 />
-                <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>    
+                <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SIGNUP'}</Button>    
             </form>
+            <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
         </Card>
     )
 };
