7914c117b68069fd2bc059db7a49ba5658beed8f
ejazhussain1995 <ejazparvi6@gmail.com>
2020-01-21 07:30:20 -0500

    Add express js error handling

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,21 +1,39 @@
-const express = require("express");
+const express = require('express');
+const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
 
-const HttpError = require('../models/http-error');
-
 const router = express.Router();
 
+router.get('/:pid', placesControllers.getPlaceById);
 
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.get("/:pid", placesControllers.getPlaceById);
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
 
-router.get("/user/:uid", placesControllers.getPlacesByUserId);
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
 
-router.post('/', placesControllers.createPlace);
-
-router.patch('/:pid', placesControllers.updatePlace);
-router.delete('/:pid', placesControllers.deletePlace)
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
-  
--- BACKEND/routes/users-routes.js
+++ BACKEND/routes/users-routes.js
@@ -1,18 +1,26 @@
-const express = require("express");
+const express = require('express');
+const { check } = require('express-validator');
 
-const usersControllers = require('../controllers/users-controllers');
-
-const HttpError = require('../models/http-error');
+const usersController = require('../controllers/users-controllers');
 
 const router = express.Router();
 
+router.get('/', usersController.getUsers);
 
+router.post(
+  '/signup',
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail() // Test@test.com => test@test.com
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersController.signup
+);
 
-router.get("/",);
-
-router.post('/signup', );
-router.post('/login', );
-
+router.post('/login', usersController.login);
 
 module.exports = router;
-  
